7036398cbeddc7cb4c42f703b495153f3ebd8f77
axelml1999 <axelin8799@gmail.com>
2020-05-04 20:42:59 -0500

    services component
    + refactoring axel code.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ import sliderGreece from './images/greece.jpg'
 import splash from './images/splash.png'
 import mouse from './images/mouse.svg'
 
+import corporate from './images/corporate.jpg'
+import cruise from './images/cruise.jpg'
+import family from './images/family.jpg'
+import fire from './images/fire.jpg'
+import flight from './images/flight.jpg'
+import hotel from './images/hotel.jpg'
+
 
 /* ********** JavaScript ********** */
 import { activeMenu, activeMenuLink } from './js/menu'
